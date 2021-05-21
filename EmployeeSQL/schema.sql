c0f856a0098ae249256c2b5b55c1c48a254ece7c
Daniel Sinda <sinda002@umn.edu>
2021-05-20 19:39:50 -0500

    sql12

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,2 +1,43 @@
-create table employees;
+titles
+-
+(emp_no INT,
+title VARCHAR(50),
+from_date DATE,
+to_date DATE,
+
+salaries
+-
+(emp_no INT 
+salary INT,
+from_date DATE,
+to_date DATE);
+
+departments
+-
+(dept_no VARCHAR(50) 
+dept_name VARCHAR(50));
+
+employees
+-
+(emp_no INT 
+PRIMARY KEY,
+birth_date DATE,
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+gender VARCHAR(50),
+hire_date DATE,
+
+dept_emp
+-
+(emp_no INT,
+dept_no VARCHAR(50),
+from_date DATE,
+to_date DATE,
+
+dept_manager
+-
+(dept_no VARCHAR(50),
+emp_no INT,
+from_date DATE,
+to_date DATE,
 
