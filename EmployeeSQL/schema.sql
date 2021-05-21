3cf3108af0ca5c1ee8de856179416d0bc3d1fd7d
Daniel Sinda <sinda002@umn.edu>
2021-05-21 13:42:02 -0500

    sql15

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,43 +1,64 @@
-titles
--
-(emp_no INT,
-title VARCHAR(50),
-from_date DATE,
-to_date DATE,
+CREATE TABLE Employees (
+    emp_no INT   NOT NULL,
+    birth_date date   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    gender VARCHAR   NOT NULL,
+    hire_date date   NOT NULL,
+	PRIMARY KEY (emp_no)
+);
 
-salaries
--
-(emp_no INT 
-salary INT,
-from_date DATE,
-to_date DATE);
+SELECT * from Employees;
 
-departments
--
-(dept_no VARCHAR(50) 
-dept_name VARCHAR(50));
+CREATE TABLE Departments (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+	PRIMARY KEY (dept_no)
+);
 
-employees
--
-(emp_no INT 
-PRIMARY KEY,
-birth_date DATE,
-first_name VARCHAR(50),
-last_name VARCHAR(50),
-gender VARCHAR(50),
-hire_date DATE,
+SELECT * from Departments;
 
-dept_emp
--
-(emp_no INT,
-dept_no VARCHAR(50),
-from_date DATE,
-to_date DATE,
+CREATE TABLE Department_Employees (
+    emp_no INT   NOT NULL,
+	dept_no VARCHAR   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
+);
 
-dept_manager
--
-(dept_no VARCHAR(50),
-emp_no INT,
-from_date DATE,
-to_date DATE,
+SELECT * from Department_Employees;
+
+CREATE TABLE Department_Manager (
+    dept_no VARCHAR   NOT NULL,
+    emp_no int   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
+);
+
+SELECT * from Department_Manager;
+
+
+CREATE TABLE Salaries (
+    emp_no int   NOT NULL,
+	salary INTEGER   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL,    
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);
+
+SELECT * from Salaries;
+
+
+CREATE TABLE Titles (
+	emp_no int   NOT NULL,
+    title VARCHAR   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);
+
+SELECT * from Titles;
 
